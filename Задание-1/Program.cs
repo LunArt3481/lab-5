28f5b2d321a4e501430b96d1f602b2e8ccac75b5
LunArt3481 <b9172148022b@gmail.com>
2026-04-01 09:31:56 +0400

    Add files via upload

---
+++ Задание-1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны два различных вещественных числа.
+            //    Определить: 
+            //    а) какое из них больше;
+            //    б) какое из них меньше.
+
+            double a = 0;
+            double b = 0;
+
+        number_a:
+
+            Console.Write($"Введите первое число: ");
+            try
+            {
+                a = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {e.Message}\nПопробуйте снова\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto number_a;
+            }
+
+        number_b:
+            Console.Write($"Введите второе число: ");
+            try
+            {
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {e.Message}\nПопробуйте снова\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto number_b;
+            }
+
+            Console.Write($"Пункт а:");
+
+            if (a > b)
+            {
+                Console.WriteLine($"Число {a} больше");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} больше");
+            }
+
+            Console.Write($"Пункт б:");
+
+            if (a < b)
+            {
+                Console.WriteLine($"Число {a} меньше ");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} больше");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание-2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // Даны радиус круга и сторона квадрата. У какой фигуры площадь больше?
+
+            double sircle_r = 0;
+            double square_r = 0;
+
+        sircle:
+            Console.Write($"Введите радиус  круга: ");
+            try
+            {
+                sircle_r = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {e.Message}\nПопробуйте снова\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto sircle;
+            }
+
+        square:
+            Console.Write($"Введите сторону квадрата: ");
+            try
+            {
+                square_r = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {e.Message}\nПопробуйте снова\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto square;
+            }
+
+            double S_square = square_r * square_r;
+            double S_sircle = sircle_r * Math.PI * 2;
+
+            Console.WriteLine($"Площадь круга = {S_sircle:F2}\nПлощадь квадрата = {S_square}");
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (S_sircle > S_square)
+            {
+                Console.WriteLine($"Площадь круга больше площади квадрата");
+            }
+            else
+            {
+                Console.WriteLine($"Площадь квадрата больше площади круга");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
